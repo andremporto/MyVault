f43f6312daf8e8f1b1c600d28db543c4f7d12330
André Porto <andreporto@me.com>
2024-05-26 07:26:53 -0300

    Initial Commit

---
+++ MyVault/MyVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVaultApp.swift
+//  MyVault
+//
+//  Created by André Porto on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MyVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
